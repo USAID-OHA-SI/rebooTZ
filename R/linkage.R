850251f64d4500db12e07b1e2e1313c4725bff0b
Aaron Chafetz <achafetz@usaid.gov>
2019-02-19 22:15:54 -0500

    remove duplicate code, update priority

--- R/linkage.R
+++ R/linkage.R
@@ -1,8 +1,8 @@
 ##  PROJECT:  rebooTZ
 ##  AUTHOR:   A.Chafetz | USAID
-##  PURPOSE:  linkage
+##  PURPOSE:  proxy linkage
 ##  DATE:     2019-02-15
-##  UPDATED:  
+##  UPDATED:  2019-02-19
 
 
 # MUNGE -------------------------------------------------------------------
@@ -62,32 +62,7 @@
          path = "Output",
          dpi = 300, 
          height = 5, width = 7, units = "in")    
-  #trend
-  df_link <- df_genie_link %>% 
-    group_by(indicator) %>% 
-    summarise_at(vars(contains("q")), sum, na.rm = TRUE) %>% 
-    ungroup() %>%
-    gather(pd, val, -indicator) %>% 
-    spread(indicator, val, fill = 0) %>% 
-    mutate(proxylink = TX_NEW / HTS_TST_POS,
-           proxylink = ifelse(is.infinite(proxylink), NA, proxylink),
-           operatingunit = "Tanzania",
-           pd = str_remove(pd, "20") %>% toupper(.))
-  
-  df_link %>% 
-    ggplot(aes(pd, proxylink, group = operatingunit, color = operatingunit)) +
-    geom_line(size = 1) +
-    geom_point(size = 5) +
-    scale_y_continuous(labels = percent_format(1)) +
-    scale_color_manual(values = "#335B8E") +
-    expand_limits(y = 0) +
-    labs(x = "", y = "proxy linkage") +
-    plot_theme()
-  
-  ggsave("TZA_linkage_trend.png", 
-         path = "Output",
-         dpi = 300, 
-         height = 5, width = 7, units = "in")    
+
   
   #trend, priority
   df_link_priority <- df_genie_link %>% 
@@ -117,7 +92,7 @@
     scale_x_discrete(labels = c("FY18Q1", rep("", 3), "FY19Q1")) +
     scale_color_manual(values = "#6CA18F") +
     expand_limits(y = 0) +
-    facet_wrap(. ~ sitename, nrow = 2) +
+    facet_wrap(. ~ sitename, nrow = 3) +
     labs(x = "", y = "proxy linkage") +
     plot_theme()
   
