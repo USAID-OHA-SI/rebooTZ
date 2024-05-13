e3c69cc64103a099a65c39d23e909cbafba28ef9
Aaron Chafetz <achafetz@usaid.gov>
2024-05-13 16:27:56 -0400

    update to datim pull

--- R/FY24Q1_TX_Attributes.R
+++ R/FY24Q1_TX_Attributes.R
@@ -1,53 +1,160 @@
-#urls
-datim_hostname = 'genie.testing.datim.org'
-api_host = 'wave.test.pdap.pepfar.net'
+# PROJECT:  rebooTZ
+# PURPOSE:  Combine TX + Site Attributes
+# AUTHOR:   A.Chafetz | USAID
+# REF ID:   a487bcd2 
+# LICENSE:  MIT
+# DATE:     2024-05-13
+# UPDATED: 
 
-#store/prompt for credentials
-accnt <- grabr::lazy_secrets("datim")
+# DEPENDENCIES ------------------------------------------------------------
+  
+  #general
+  library(tidyverse)
+  library(glue)
+  library(janitor, warn.conflicts = FALSE)
+  #oha
+  library(gagglr) ##install.packages('gagglr', repos = c('https://usaid-oha-si.r-universe.dev', 'https://cloud.r-project.org'))
+  library(grabr)  
+  #viz extensions
+  library(scales, warn.conflicts = FALSE)
+  library(systemfonts)
+  library(tidytext)
+  library(patchwork)
+  library(ggtext)
 
-#establish DATIM session
-datim_results <- httr::GET(
-  stringr::str_glue('https://{datim_hostname}/api/me'),
-  httr::authenticate(accnt$username, accnt$password, type = "basic")
-)
+# GLOBAL VARIABLES --------------------------------------------------------
+  
+  load_secrets("datim")
 
-#establish DATIM API session
-api_login_result <- httr::GET(
-  stringr::str_glue('https://{api_host}/api/Authenticate/datim'),
-  httr::set_cookies(JSESSIONID = datim_results$cookies$value),
-  query=list(datim_hostname=datim_hostname)
-)
+  ref_id <- "a487bcd2"  #a reference to be places in viz captions 
+  
+  cntry <- "Tanzania"
+  
+# PDAP WAVE API -----------------------------------------------------------
 
-#country 
-cntry_uid <- stringr::str_sub(post_body$uid_hierarchy_list[[1]], start = -11)
+  # #get TZA UID
+  # cntry_uid <- pepfar_country_list %>%
+  #   filter(country == cntry) %>%
+  #   pull(country_uid)
+  # 
+  # #establish parameters to pass into POST API
+  # post_body <- list(
+  #   daily_frozen='daily',
+  #   fiscal_year=list(2024),
+  #   funding_agency = list("USAID"),
+  #   indicator=list("TX_CURR"),
+  #   standardizeddisaggregate = list("Total Numerator"),
+  #   uid_hierarchy_list=list(stringr::str_glue('-|-|{cntry_uid}')))
+  # 
+  # #run API
+  # wave_process_query(post_body, psd_type = "site_im", request_type = "GET")
+  # 
+  # genie_path <- return_latest("Data", "Genie")
+  # 
+  # meta <- get_metadata(genie_path, caption_note = "USAID")  
 
-#country
-cntry <- glamr::pepfar_country_list %>% 
-  dplyr::filter(country_uid == cntry_uid) %>% 
-  dplyr::pull(country) %>% 
-  stringr::str_remove_all("( |')")
+# DATIM ATTRIBUTES --------------------------------------------------------
+  
+  # #get sql view table uid
+  # sqlview_name <- datim_sqlviews() %>% 
+  #   filter(str_detect(name, "Attributes")) %>% 
+  #   pull(name)
+  # 
+  # #get iso code
+  # cntry_iso <- pepfar_country_list %>%
+  #   filter(country == cntry) %>%
+  #   pull(country_iso)
+  # 
+  # # Extract Organisation Units data for a specific country - All levels with child / parent links
+  # df_attr <- datim_sqlviews(view_name = {sqlview_name}, 
+  #                            dataset = TRUE,
+  #                            query = list(type = "variable", params = list("OU" = cntry_iso)))
+  
+# IMPORT ------------------------------------------------------------------
+  
+  # df_genie <- read_psd(genie_path) 
+  
 
-#ensure only one psd_type
-psd_type <- "site_ims"
+# DATIM API ---------------------------------------------------------------
 
-#string type
-psd_type_string <- psd_type %>% 
-  stringr::str_extract("^.*(?=_)") %>% 
-  toupper()
+  info <- get_outable() %>% 
+    filter(country == cntry) %>% 
+    select(country, country_uid, facility_lvl) %>% 
+    as.list()
+  
+  
+  url <- paste0("https://final.datim.org/api/analytics.json?",
+                "dimension=ou:", info$country_uid, ";LEVEL-", info$facility_lvl, "&", #hierarchy
+                "filter=RUkVjD3BsS1&", #top level
+                "dimension=bw8KHXzxd9i&", #Funding Agency
+                "dimension=CH5v24DUJO0&", #Site Attribute: Ownership Type
+                "dimension=SbeNLojYo3t&", #Site Attribute: Facility Type
+                "dimension=LxhLO68FcXm:MvszPTQrUhy&", #indicator: TX_CURR
+                "dimension=pe:2021Oct;2022Oct;2023Oct&", #periods
+                "displayProperty=SHORTNAME&skipMeta=false&hierarchyMeta=false")
+  
+  df_datim <- datim_process_query(url)
+  
+  df_datim_clean <- df_datim %>% 
+    rename(orgunit = `Organisation unit`,
+           funding_agency = `Funding Agency`,
+           ownership_type = `SA Own Type`,
+           facility_type = `SA Facility Type`,
+           indicator = `Technical Area`) %>% 
+    rename_with(tolower) %>% 
+    clean_agency() %>% 
+    mutate(ownership_type = str_remove(ownership_type, "^OWN "),
+           facility_type = str_remove(facility_type, "^FT ")
+           ) %>% 
+    mutate(fiscal_year = period %>% str_sub(-4) %>% as.numeric(), .before = value) %>% 
+    arrange(fiscal_year) %>% 
+    select(-period) %>% 
+    pivot_wider(names_from = c(indicator, fiscal_year),
+                names_glue = "{tolower(indicator)}_fy{str_sub(fiscal_year, -2)}")
+  
+  df_orgs <- datim_orgunits(info$country, reshape = TRUE) %>% 
+    select(orgunituid, country, snu1)
+  
+  df_datim_clean <- df_datim_clean %>% 
+    right_join(df_orgs, .) %>% 
+    relocate(orgunit, 1)
+  
+  write_csv(df_datim_clean, "Dataout/TZA_TX_SA.csv", na  = "")
+      
 
-#output path 
-zip_path <- file.path(folderpath_dwnld, 
-                      stringr::str_glue('Genie-{psd_type_string}ByIMs-{cntry}-Daily-{Sys.Date()}.zip'))
+# MUNGE -------------------------------------------------------------------
 
-#DATIM POST request
-httr::POST(
-  stringr::str_glue('https://{api_host}/api/data/{psd_type}'),
-  httr::set_cookies(wave_session = api_login_result$cookies$value),
-  encode='json',
-  body={post_body},
-  httr::write_disk(zip_path, overwrite=TRUE)
-)
+  #aggregate to one observation by site
+  # df_sites <- df_genie %>%
+  #   filter(funding_agency == "USAID") %>%
+  #   group_by(fiscal_year, snu1, sitetype, sitename, orgunituid, mech_code, indicator) %>%
+  #   summarise(cumulative = sum(cumulative, na.rm = TRUE),
+  #             .groups = "drop")
 
-return_latest("Data", "Genie") %>% 
-  read_psd() %>% 
-  glimpse()
+  #clean columns
+  # df_attr <- df_attr %>% 
+  #   clean_names() %>%
+  #   select(orgunituid = datim_uid,
+  #          facility_type:clinic_hours)%>%
+  #   mutate(across(facility_type:clinic_hours,  \(x) na_if(x, "")))
+  
+  #join with attributes
+
+
+# VIZ ---------------------------------------------------------------------
+  
+  df_datim_clean %>% 
+    count(funding_agency, ownership_type) %>% 
+    mutate(ownership_type = fct_reorder(ownership_type, n, sum)) %>% 
+    arrange(desc(ownership_type)) %>% 
+    pivot_wider(names_from = funding_agency, values_from = n)
+  
+  df_datim_clean %>% 
+    count(funding_agency, ownership_type, wt = tx_curr_fy24) %>% 
+    mutate(ownership_type = fct_reorder(ownership_type, n, sum)) %>% 
+    arrange(desc(ownership_type)) %>%
+    pivot_wider(names_from = funding_agency, values_from = n)
+  
+  
+  df_datim_clean %>% 
+    count(funding_agency, wt = tx_curr_fy24) 
--- R/pdap_pull.R
+++ R/pdap_pull.R
@@ -1,13 +1,27 @@
 #' Pull Genie Data from PDAP Wave
+#' 
+#' This function provides streamlined API access to PDAP Wave data, the
+#' successor to DATIM Genie. PDAP Wave API simplifies the requests that 
+#' previously needed to be made with DATIM and returns a dataset back that 
+#' matches the MSD structure. Further documentation can be found at 
+#' \url{https://wave.test.pdap.pepfar.net/api/docs#/}. 
+#' 
+#' Users must pass their query filter in a list form into \code{request_body}, 
+#' which matches what you would manually do in Genie previously. You can proceed
+#' with either POST or GET requests to access PSNUxIM, OUxIM, and SitexIM data.
+#' 
+#' This function was adapted from code developed and shared by Derek Wood 
+#' (GHSD/PRIME).
 #'
-#' @param post_body elements to pass into the PDAP Wave POST API
+#' @param request_body elements to pass into the PDAP Wave POST API
 #' @param folderpath_dwnld where to download, default = "Data"
 #' @param psd_type Type of PEPFAR Structured dataset: "psnu_im" (default), 
 #'   "ou_im", or "site_im"
+#' @param request_type API request type: "POST" (default) or "GET
 #' @param username DATIM username, if blank looks for stored credentials 
-#'   (glamr::set_datim) and then prompts for credentials if not found 
+#'   (\code{glamr::set_datim()}) and then prompts for credentials if not found 
 #' @param password DATIM password, if blank looks for stored credentials 
-#'   (glamr::set_datim) and then prompts for credentials if not found
+#'   (\code{glamr::set_datim()}) and then prompts for credentials if not found
 #'
 #' @return list of request and stored data in zip
 #' @export
@@ -31,7 +45,7 @@
 #'                   "TX_RTT","PMTCT_STAT", "PMTCT_STAT_POS", "PMTCT_ART"),
 #'    uid_hierarchy_list=list(str_glue('-|-|{cntry_uid}')))
 #'    
-#'  #run API
+#'  #run POST API
 #'  pdap_pull(cntry_uid, post_body)
 #'
 #'  #load data
@@ -39,15 +53,74 @@
 #'     read_psd(zip_path)
 #'    }
   
-pdap_pull <- function(post_body,
-                      folderpath_dwnld = "Data",
-                      psd_type = c("psnu_im", "ou_im", "site_im"),
-                      username,
-                      password){
+pdap_request <- function(request_body,
+                         folderpath_dwnld = "Data",
+                         psd_type = c("psnu_im", "ou_im", "site_im"),
+                         request_type = c("POST", "GET"),
+                         username,
+                         password){
   
-  #urls
-  datim_hostname = 'genie.testing.datim.org'
-  api_host = 'wave.test.pdap.pepfar.net'
+  #establish session
+  sess_token <- est_session(username, password)
+  
+  #ensure only one request_type
+  request_type <- request_type[1]
+  request_accepted <-  c("POST", "GET")
+  if(!request_type %in% request_accepted)
+    cli::cli_abort("The provided {.code request_type} is not one of the accepted inputs: {.code {request_accepted}}")
+  
+  #ensure only one psd_type
+  psd_type <- psd_type[1]
+  psd_accepted <-  c("psnu_im", "ou_im", "site_im")
+  if(!psd_type %in%  psd_accepted)
+    cli::cli_abort("The provided {.code psd_type} is not one of the accepted inputs: {.code {psd_accepted}}")
+  
+  #export path
+  zip_path <- est_export_path(post_body, folderpath_dwnld, psd_type)
+  
+  #submit request & store data locally
+  if(request_type == "POST"){
+    #DATIM POST request
+    httr::POST(
+      stringr::str_glue('https://{api_host}/api/data/{psd_type}'),
+      httr::set_cookies(wave_session = api_login_result$cookies$value),
+      encode='json',
+      body={post_body},
+      httr::write_disk(zip_path, overwrite=TRUE)
+    )
+  } else {
+    #DATIM GET request
+    httr::GET(
+      stringr::str_glue('https://{api_host}/api/data/{psd_type}'),
+      httr::set_cookies(wave_session = api_login_result$cookies$value),
+      query=flattenbody({post_body}),
+      httr::write_disk(zip_path, overwrite=TRUE)
+    )
+  }
+  
+  
+  #notification
+  cli::cli_alert_info("The PDAP Wave successfully executed and the output is saved as {.file {zip_path}}")
+  
+  invisible()
+  
+  
+}
+
+#' Establish PDAP Wave Session
+#'
+#' @inheritParams pdap_request
+#' @param datim_hostname DATIM URL: 'genie.testing.datim.org'
+#' @param api_host API address: 'wave.test.pdap.pepfar.net'
+#'
+#' @return session token
+#' @keywords internal
+
+est_session <- function(username,
+                        password,
+                        datim_hostname = 'genie.testing.datim.org',
+                        api_host = 'wave.test.pdap.pepfar.net'
+                        ){
   
   #store/prompt for credentials
   accnt <- grabr::lazy_secrets("datim", username, password)
@@ -65,40 +138,87 @@ pdap_pull <- function(post_body,
     query=list(datim_hostname=datim_hostname)
   )
   
-  #country 
-  cntry_uid <- stringr::str_sub(post_body$uid_hierarchy_list[[1]], start = -11)
+  session_token <- api_login_result$cookies$value
   
-  #country
-  cntry <- glamr::pepfar_country_list %>% 
-    dplyr::filter(country_uid == cntry_uid) %>% 
-    dplyr::pull(country) %>% 
-    stringr::str_remove_all("( |')")
-  
-  #ensure only one psd_type
-  psd_type <- psd_type[1]
+  return(session_token)
+}
+
+
+#' Establish Export Path
+#'
+#' @inheritParams pdap_request
+#' @param cntry_name country name
+#'
+#' @return vector of the filename
+#' @keywords internal
+#' 
+est_export_path <- function(post_body, folderpath_dwnld, psd_type){
   
   #string type
-  psd_type_string <- psd_type %>% 
-    stringr::str_extract("^.*(?=_)") %>% 
-    toupper()
+  psd_type_string <- stringr::str_extract(psd_type, "^.*(?=_)") %>% toupper()
+  
+  #country
+  cntry <- extract_cntry(post_body)
+  
+  #string daily_frozen
+  daily_frozen <- stringr::str_to_sentence(post_body$daily_frozen)
   
   #output path 
   zip_path <- file.path(folderpath_dwnld, 
-                        stringr::str_glue('Genie-{psd_type_string}ByIMs-{cntry}-Daily-{Sys.Date()}.zip'))
+                        stringr::str_glue('PDAPWave-{psd_type_string}ByIMs-{cntry}-{daily_frozen}-{Sys.Date()}.zip'))
   
-  #DATIM POST request
-  httr::POST(
-    stringr::str_glue('https://{api_host}/api/data/{psd_type}'),
-    httr::set_cookies(wave_session = api_login_result$cookies$value),
-    encode='json',
-    body={post_body},
-    httr::write_disk(zip_path, overwrite=TRUE)
-  )
+  return(zip_path)
+}
+
+#' Extract Country Name
+#'
+#' @inheritParams pdap_request
+#'
+#' @return vector country's name
+#' @keywords internal
+#' 
+extract_cntry <- function(request_body){
   
-  #notification
-  cli::cli_alert_info("The PDAP Wave successfully executed and the output is saved as {.file {zip_path}} ")
+  #country uid
+  uid <- stringr::str_sub(post_body$uid_hierarchy_list[[1]], start = -11)
   
-  invisible()
+  if(is.null(cntry_uid))
+    cli::cli_abort("Missing country UID in {.field request_body}, which should be structured like {.code uid_hierarchy_list=list(stringr::str_glue('-|-|cntry_uid')}")
   
+  #country name
+  name <- glamr::pepfar_country_list %>% 
+    dplyr::filter(country_uid == uid) %>% 
+    dplyr::pull(country) %>% 
+    stringr::str_remove_all("( |')")
   
+  if(length(name) == 0)
+    cli::cli_abort("Check the OU UID in {.field request_body} to ensure it is valid")
+  
+  return(name)
+}
+
+
+#' Flatten Body
+#' 
+#' A query can only have one value per name, so take
+#' any values that contain vectors length >1 and
+#' split them up. For example list(x=1:2, y="a") becomes list(x=1, x=2, y="a").
+#' This code was developed by Derek Wood (GHSD/PRIME) 
+#'
+#' @param x list
+#' @keywords internal
+flattenbody <- function(x) {
+
+  if (all(lengths(x)<=1)) return(x);
+  do.call("c", mapply(function(name, val) {
+    if (length(val)==1 || any(c("form_file", "form_data") %in% class(val))) {
+      x <- list(val)
+      names(x) <- name
+      x
+    } else {
+      x <- as.list(val)
+      names(x) <- rep(name, length(val))
+      x
+    }
+  }, names(x), x, USE.NAMES = FALSE, SIMPLIFY = FALSE))
 }
